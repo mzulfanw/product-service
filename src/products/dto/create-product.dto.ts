521919f3e28d17fc4762e023bfb69c503727c36b
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2025-10-26 19:53:22 +0700

    feat: added validation on create products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
 
 export class CreateProductDto {
   @IsString()
@@ -6,8 +6,10 @@ export class CreateProductDto {
   name: string;
 
   @IsNumber()
+  @Min(1)
   price: number;
 
+  @Min(1)
   @IsNumber()
   qty: number;
 }
